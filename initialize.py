6c7df806a1cb833a2f0c1796bd06aef11e8f40d1
Felix <felixvillage44@gmail.com>
2025-04-15 11:53:00 -0400

    tested

---
+++ initialize.py
@@ -0,0 +1,30 @@
+import yaml
+from main_logging import Sensor
+
+
+with open('Sensor_Info.yml') as f:
+    sensor_info = yaml.load(f, Loader=yaml.FullLoader)
+
+sensor_list = list(sensor_info.keys())
+
+#threads = []
+sensors = []
+
+for sensor in sensor_list:
+
+    name = sensor_info[sensor]["Name"]
+    rate = sensor_info[sensor]["Rate"]
+    port = sensor_info[sensor]["Port"]
+    baud = sensor_info[sensor]["Baud"]
+    return_symbol = sensor_info[sensor]["Return_Symbol"]
+    extra_char = sensor_info[sensor]["Extra_Char"]
+    read_line = sensor_info[sensor]["Read_Line"]
+
+    with open('sensor_data/'+name+'.csv', 'w') as creating_new_csv_file: 
+        pass 
+    print("Empty File Created Successfully")
+
+    sensor = Sensor(name,rate,port,baud,return_symbol,extra_char,read_line)
+    sensors.append(sensor)
+    Sensor.new_thread(sensor)
+
